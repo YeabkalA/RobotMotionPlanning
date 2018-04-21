8bb64def4ae140178ea885716fa6673dafc8f92d
Yeabkal <ywubshit@udallas.edu>
2018-04-21 02:00:28 -0500

    Add Polygon and Vector classes

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,6 +1,92 @@
-public class Polygon {
-    
+import java.util.ArrayList;
+
+/**
+ * Polygon
+ * described as list of vectors making up the polygon
+ */
+static class Polygon{
+
     private ArrayList<Vector> vectors = new ArrayList<>();
 
     public void addVector(Vector v){
+	this.vectors.add(v);
+    }
+
+    public Polygon(){
+    }
+
+    public ArrayList<Vector> getVectors(){
+	return this.vectors;
+    }
+
+    /**
+     * @param vertices the array of {x1,y1,x2,y2,...} values making up the polygon
+     *Constructs Polygon from array of x,y coordinates of its vertices
+     */
+    public Polygon(double[] vertices){
+	for(int i=0;i<vertices.length-3;i+=2){
+	    double x1 = vertices[i];
+	    double y1 = vertices[i+1];
+	    double x2 = vertices[i+2];
+	    double y2 = vertices[i+3];
+	    Vector vec = new Vector(x1,y1,x2,y2);
+	    this.vectors.add(vec);
+	}
+	this.vectors.add(new Vector(vertices[vertices.length-2],vertices[vertices.length-1],vertices[0],vertices[1]));
+    }
+
+    /**
+     * Prints the polygon 
+     */
+    public void print(){
+	for(Vector v: this.vectors){
+	    System.out.println("From " + v.x1 + "," + v.y1 + " to " + v.x2 + "," + v.y2);
+	}
+    }
+
+    /**
+     * @return the position vectors derived from the vectors making up the polygon
+     */
+    public Vector[] getPosVecs(){
+	Vector[] ret = new Vector[this.vectors.size()];
+	int count = 0;
+	for(Vector v: this.vectors){
+	    ret[count] = v.getPosVector();
+	    count++;
+	}
+	return ret;
+    }
+
+    /**
+     * @param p polygon with whom this is calculated for Minkowski sum
+     * @return the Minkowski sum of this with p
+     */
+    
+    public Polygon getMinkowski(Polygon p){
+	Vector[] posVecs = this.getPosVecs();
+	Vector[] posSec = p.getPosVecs();
+
+	Vector[] posVectorsMink = new Vector[posVecs.length + posSec.length];
+	for(int i=0;i<posVecs.length;i++) posVectorsMink[i] = posVecs[i];
+	int count = posVecs.length;
+	for(int i=0;i<posSec.length;i++){
+	    posVectorsMink[count] = posSec[i];
+	    count++;
+	}
+
+	Arrays.sort(posVectorsMink,Comparator.comparing(s->s.getAngle()));
+	for(Vector v: posVectorsMink) v.print();
+	System.out.println("End of check");
+
+	int fir=0,sec=0;
+
+	Polygon MinkowskiSum = new Polygon();
+	MinkowskiSum.addVector(posVectorsMink[0]);
+	for(int i=1;i<posVectorsMink.length;i++){
+	    Vector previous = MinkowskiSum.getVectors().get(i-1);
+	    MinkowskiSum.add(new Vector(previous.getx2(), previous.gety2(),previous.getx2()+posVectorsMink[i].getX(), previous.gety2()+posVectorsMink[i].getY()));
+	}
+	return MinkowskiSum;
+    }
+}
 	
